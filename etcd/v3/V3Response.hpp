4caa739834516eb4e842f858a4cb61d08509ccdd
Sungsik <sungsik@ultihash.io>
2025-06-02 22:43:41 +0200

    Handle multiple events returned by recursive watch

--- etcd/v3/V3Response.hpp
+++ etcd/v3/V3Response.hpp
@@ -11,7 +11,7 @@
 namespace etcdv3 {
 class V3Response {
  public:
-  V3Response() : error_code(0), index(0){};
+  V3Response() : error_code(0), index(0) {};
   void set_error_code(int code);
   int get_error_code() const;
   std::string const& get_error_message() const;
@@ -19,6 +19,7 @@ class V3Response {
   void set_action(std::string action);
   int64_t get_index() const;
   std::string const& get_action() const;
+  std::vector<std::string> const& get_actions() const;
   std::vector<etcdv3::KeyValue> const& get_values() const;
   std::vector<etcdv3::KeyValue> const& get_prev_values() const;
   etcdv3::KeyValue const& get_value() const;
@@ -46,6 +47,7 @@ class V3Response {
   std::string action;
   etcdv3::KeyValue value;
   etcdv3::KeyValue prev_value;
+  std::vector<std::string> actions;
   std::vector<etcdv3::KeyValue> values;
   std::vector<etcdv3::KeyValue> prev_values;
   int64_t compact_revision = -1;
